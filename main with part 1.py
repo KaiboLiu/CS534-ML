d3b3ea101a603f860a5c49ac356580bcbeb77887
KaiboLiu <liukaibo87@gmail.com>
2016-10-06 01:24:37 -0700

    Update main with part 1.py
    Part 1 optimized the learning rate to 0.5, with a minimum loss function to 372.12

--- main with part 1.py
+++ main with part 1.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 """
 Created on Mon Oct 03 22:05:52 2016
-
 @author: Kaibo Liu
 """
+
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
@@ -31,9 +31,7 @@ def get_colour(color):
 
 
 def HW1_part_1(trainX,trainY,grad_epsilon=0.01,max_iterations=100000):
-    print trainX,trainY
-    learning_rate  = [0.001,0.003,0.01,0.03,0.04,0.05]#,0.06,0.1]
-#    lambda_reg     = [0, 0.0001, 0.001, 0.01, 1, 10, 100]
+    learning_rate  = [0.01,0.03,0.05,0.06]
     color = 0
     plt.figure(1)
     for lr in learning_rate:
@@ -41,23 +39,23 @@ def HW1_part_1(trainX,trainY,grad_epsilon=0.01,max_iterations=100000):
         plt.figure(1)
 #        plt.plot(range(1,step+2),lossCont, get_colour(color),label="learning_rate %s" % str(lr))
         plt.plot(lossCont, get_colour(color),label="learning_rate %s" % str(lr))
- 
         color += 1
 #        title("learning_rate %s" % str(lr))
-        plt.xlim(0, 400)
-        plt.ylim(0, 60000)
-  #      plt.ylim(0, 1e100)
+        plt.xlim(0, 2000)
+        plt.ylim(200, 2000)
         xlabel('iteration')
         ylabel('loss J(w)')  
         plt.legend(loc='upper right')
         grid(True)  
-        savefig("Part1_converged Loss with learning rate_scaled")
+    savefig("Part1_converged Loss with learning rate_scaled-Para")
+    print 'para:MaxIter=%d,Normalization=ZscoreNorm,L2-Norm<epsilon=%s,LossThreshold=1e100,grad/n' % (max_iterations,str(grad_epsilon))
+#        savefig("Part1_unconverged Loss-Para")
 
         #    savefig("Part1_Loss-iter with learning_rate")  
     plt.show()
     
-    bestloss = np.min(lossCont)
-    lr = 1
+    bestloss_Part1 = np.min(lossCont)
+    lr = 0.05 #According to the convergence and figure results above
     return lr,bestloss,wght_hist,lossCont
 
 def run():
@@ -69,15 +67,43 @@ def run():
     # part2: fixed learning rate, different lamda
     [trainX, testX] = nor.Normalization(trainX, testX)
 #    learning_rate  = [0.001,0.03,0.1,0.3,1,3,10,30,100]
+    learning_rate  = [0.001, 0.01, 0.1, 1, 10, 100]
+    max_iterations = 100000
+    grad_epsilon   = 0.001
+    lambda_reg     = [0, 0.0001, 0.001, 0.01, 1, 10, 100]
 
     '''part 1'''
     [lr,bestloss,weight,lossCont] = HW1_part_1(trainX,trainY) #default lr,grad_epsilon and max_iterations
 
     '''
+    final_loss     = []
+    for lmd in lambda_reg:
+        [weight_hist, lossCont, predictCont] = gd.GradientDescent(trainX, trainY, learning_rate, grad_epsilon, max_iterations, lmd)
+        loss = lossCont[-1] #record the final loss.
+        final_loss.append(loss)
 
+        print("Loss convergence history is: \n", lossCont)
+        Output2File(weight_hist, lossCont, learning_rate, lmd)
+    print("for diff lambda, their final loss are:\n", final_loss)
+'''
     print 'Loading time: %.4f s' % (tl-t0)
     print 'Computing time:%.4f s' % (float(time.clock())-t0)
 
+
+    # figure show.
+'''
+    idx = []
+    for i in range(1, 51):
+        idx.append(i)
+
+    print idx
+
+    plt.figure(1)
+    plt.title("Accuracy for Given lr in Gaussian Descent")
+    plt.plot(idx, lossCont)
+    plt.show()
+
+'''
 
 
 if __name__ == '__main__':
