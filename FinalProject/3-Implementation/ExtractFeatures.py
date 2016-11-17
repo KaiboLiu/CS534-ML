760222e9d75768d77f611da26637eaba2f09444e
Eugene Seo <seoe@oregonstate.edu>
2016-11-17 00:26:16 -0800

    merge all audio features (204 dim)

--- FinalProject/3-Implementation/ExtractFeatures.py
+++ FinalProject/3-Implementation/ExtractFeatures.py
@@ -3,6 +3,17 @@ import scipy.io.wavfile
 from scikits.talkbox.features import mfcc
 import matplotlib.pyplot as plt
 import librosa
+import pandas
+
+def LoadData():
+    # read input data for training & testing
+    dataDir    = "./Data/"
+    trainFile  = "train.txt"
+    testFile   = "test.txt"
+    train = pandas.read_csv(dataDir+trainFile, sep=",", names=["filename","label"])
+    test = pandas.read_csv(dataDir+testFile, sep=",", names=["filename","label"])
+
+    return [train.filename, train.label, test.filename, test.label]
 
 def Written2File(fileName, vecV):
 	f = open(fileName, "w")
@@ -51,64 +62,93 @@ def ExtreactMFCC(train, test):
 
 	return train_feature, test_feature
 
-def ExtractFeaturesByLibrosa(train, test):
-	for i in range(len(train)):
-		print "\nfilename:", train[i]
-		y, sr = librosa.load(train[i])
+def ExtractFeaturesByLibrosa(sample, label, filePath):
+	sampleF = []
 
+	for i in range(len(sample)):
+		print i
+		feature = []
+		filename = filePath + sample[i].astype('str') + ".wav"
+		y, sr = librosa.load(filename)
+		'''
 		tempo, beat_frames = librosa.beat.beat_track(y=y, sr=sr)
 		print "tempo:", tempo
 
 		beat_times = librosa.frames_to_time(beat_frames, sr=sr)
 		print "beat_times:", beat_times.shape
-
+		'''
 		chromagram = librosa.feature.chroma_stft(y, sr=sr)
-		print "chromagram:", chromagram.shape
+		#print "chromagram:", np.average(chromagram, 1)
+		feature.extend(np.average(chromagram, 1))
 
 		Qchromagram = librosa.feature.chroma_cqt(y, sr=sr)
-		print "Qchromagram:", Qchromagram.shape
+		#print "Qchromagram:", np.average(Qchromagram, 1)
+		feature.extend(np.average(Qchromagram, 1))
 
 		chromagram_cens = librosa.feature.chroma_cens(y, sr=sr)
-		print "chromagram_cens:", chromagram_cens.shape
+		#print "chromagram_cens:", chromagram_cens.shape
+		feature.extend(np.average(chromagram_cens, 1))
 
 		melspec = librosa.feature.melspectrogram(y, sr=sr)
-		print "melspectrogram:", melspec.shape
+		#print "melspectrogram:", melspec.shape
+		feature.extend(np.average(melspec, 1))
 
 		mfcc = librosa.feature.mfcc(y, sr=sr)
-		print "mfcc:", mfcc.shape
+		#print "mfcc:", mfcc.shape
+		feature.extend(np.average(mfcc, 1))
 
 		rmse = librosa.feature.rmse(y)
-		print "rmse:", rmse.shape
+		#print "rmse:", rmse.shape
+		feature.extend(np.average(rmse, 1))
 
 		centroid = librosa.feature.spectral_centroid(y, sr=sr)
-		print "spectral_centroid:", centroid.shape
+		#print "spectral_centroid:", centroid.shape
+		feature.extend(np.average(centroid, 1))
 
 		bandwidth = librosa.feature.spectral_bandwidth(y, sr=sr)
-		print "spectral_bandwidth:", bandwidth.shape
+		#print "spectral_bandwidth:", bandwidth.shape
+		feature.extend(np.average(bandwidth, 1))
 
 		contrast = librosa.feature.spectral_contrast(y, sr=sr)
-		print "spectral_contrast:", contrast.shape
+		#print "spectral_contrast:", contrast.shape
+		feature.extend(np.average(contrast, 1))
 
 		rolloff = librosa.feature.spectral_rolloff(y, sr=sr)
-		print "rolloff:", rolloff.shape
+		#print "rolloff:", rolloff.shape
+		feature.extend(np.average(rolloff, 1))
 
 		poly_features = librosa.feature.poly_features(y, sr=sr)
-		print "poly_features:", poly_features.shape
+		#print "poly_features:", poly_features.shape
+		feature.extend(np.average(poly_features, 1))
 
 		tonnetz = librosa.feature.tonnetz(y, sr=sr)
-		print "tonnetz:", tonnetz.shape
+		#print "tonnetz:", tonnetz.shape
+		feature.extend(np.average(tonnetz, 1))
 
 		zero_crossing_rate = librosa.feature.zero_crossing_rate(y)
-		print "zero_crossing_rate:", zero_crossing_rate.shape
+		#print "zero_crossing_rate:", zero_crossing_rate.shape
+		feature.extend(np.average(zero_crossing_rate, 1))
 
+		if label[i].find("cmn") == 0:
+			feature.append(0)
+		elif label[i].find("eng") == 0:
+			feature.append(1)
+
+		sampleF.append(feature)
+
+	return np.array(sampleF)
 
 if __name__ == "__main__":
-	train = np.array(["./Data/train/chinese1.wav","./Data/train/chinese2.wav","./Data/train/english1.wav","./Data/train/english2.wav"])
-	test = np.array(["./Data/test/chinese3.wav","./Data/test/chinese4.wav","./Data/test/english3.wav","./Data/test/english4.wav"])
 	#[trainF, testF] = ExtreactMFCC(train, test)
-	ExtractFeaturesByLibrosa(train, test)
+	[trainX, trainY, testX, testY] = LoadData()
+
+	fileDir    = "./Data/"
+	trainFolder  = "train/"
+	testFolder   = "test/"
+	trainF = ExtractFeaturesByLibrosa(trainX, trainY, fileDir+trainFolder)
+	testF = ExtractFeaturesByLibrosa(testX, testY, fileDir+testFolder)
 
 	trainFileName = "./Feature/train.dev"
 	testFileName  = "./Feature/test.dev"
-	#Written2File(trainFileName, trainF)
-	#Written2File(testFileName, testF)
+	Written2File(trainFileName, trainF)
+	Written2File(testFileName, testF)
